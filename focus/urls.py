d9a9d16d0a278ca5d9e32c359e02a571d38f3e22
energy1010 <energy1010@github.com>
2018-04-14 23:46:44 +0800

    update urls.py

--- focus/urls.py
+++ focus/urls.py
@@ -11,6 +11,19 @@ urlpatterns = [
     url(r'^logout/$', views.log_out, name='logout'),
 
 
+	#TODO
+	#分类列表页面
+	url(r'^(?P<cate_name>\w+)/$', views.cate-list, name='cate-list')
+
+	#内行人列表页面
+	url(r'^persons/', views.cate-list, name='cate-list')
+
+	#内行人搜索页面
+	url(r'^persons-search/', views.persons-search, name='persons-search')
+
+	#内行人详情页面
+	url(r'^persons-detail/', views.persons-detail, name='persons-detail')
+
 
     # url(r'^(?P<article_id>[0-9]+)/comment/$', views.comment, name='comment', namespace='focus'),
     # url(r'^(?P<article_id>[0-9]+)/keep/$', views.get_keep, name='keep', namespace='focus'),
