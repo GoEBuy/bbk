28bc7035188e448787950b9c4cac220790018b00
energy1010 <energy1010@github.com>
2018-04-15 17:37:16 +0800

    udpate writeDb.py

--- writeDb.py
+++ writeDb.py
@@ -25,15 +25,15 @@ def getEntityCount(entity):
 
 
 def addUsers():
-	pdb.set_trace()
 	#count = NewUser.objects.all().count()
 	#print NewUser.objects.all()
 	count = getEntityCount(NewUser.objects)
 	print "count:%d" % (count)
 
-	insertUsers = [("admin","admin"), ("root","root"), ("yangyuanyang","yangyuanyang"), ("a","a")]
+	insertUsers = [("admin","admin"), ("root","root"), ("yyy", "yyy"), ("yyf", "yyf"), ("yangyuanyang","yangyuanyang"), ("a","a")]
 	for i in xrange(len(insertUsers) ):
 		name, pwd = insertUsers[i]
+		#NewUser.objects.getorcreate()
 		if not NewUser.getUser(name, pwd):
 			NewUser.objects.create(username=name, password=pwd, pwd=pwd,  phone="15011033945", city="beijing", email="8678@qq.com" )
 		else:
@@ -53,7 +53,7 @@ def addCate():
 	count = getEntityCount(Category.objects)
 	print "count:%d" % (count)
 
-	pdb.set_trace()
+
 	root_cate=Category.objects.get(pk=-1)
 
 	insertCates = [(1, root_cate, "transport", "交通运输" ), (2, root_cate, "house", "房产销售" ), (3, root_cate, "bussiness", "电脑办公"), (4, root_cate, "guide", "当地向导")]
@@ -91,13 +91,41 @@ def addCate():
 
 def addUserPref():
 	""" 用户内行专业CRUD """
+	manager = UserStar.objects
+	count = getEntityCount(UserStar.objects)
+	print "count:%d" % (count)
+
+	user = NewUser.getUser("admin","admin")
+	
+	
+	cate1=Category.objects.get(pk=1)
+	cate2=Category.objects.get(pk=2)
+	cate3=Category.objects.get(pk=3)
+	insertCates1 = [(cate1, 3, 2, "cate 1 desc"), (cate2, 1, 5, "cate 2 desc" ), (cate3, 2,5, "cate 3 desc") ]
+
+	for i in xrange(len(insertCates1) ):
+		print insertCates1[i]
+		cate, star_service , star_personal, desc = insertCates1[i]
+		try:
+			manager.create(user=user, cate = cate, star_personal=star_personal, star_service = star_service, desc = desc)
+			#Category.objects.get(pk=cate_id)
+			#print "category:%s exists" %(cate_id)
+		except Exception as e:
+			print "err", e
+			continue
+
+	count = getEntityCount( UserStar.objects)
+	print "count:%d" % (count)
+
+
 	pass
 
 
 def main():
 	#addUsers()
 
-	addCate()
+	#addCate()
+	addUserPref()
 
 if __name__ == "__main__":
 	
